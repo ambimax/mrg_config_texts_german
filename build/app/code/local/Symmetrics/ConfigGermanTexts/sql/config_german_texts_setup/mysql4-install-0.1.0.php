e37c9e2db470460f820187718d8d9ed518f588b8
Eugen Gitin <devnull@localhost>
2009-06-01 15:27:05 +0200

    Übersetzung für "Subtotal" geändert; Übersetzung für "Tax" geändert;

--- build/app/code/local/Symmetrics/ConfigGermanTexts/sql/config_german_texts_setup/mysql4-install-0.1.0.php
+++ build/app/code/local/Symmetrics/ConfigGermanTexts/sql/config_german_texts_setup/mysql4-install-0.1.0.php
@@ -601,7 +601,8 @@ INSERT INTO `core_translate` (`string`, `store_id`, `translate`, `locale`) VALUE
 ('Mage_Newsletter::Sign up for our newsletter:', 0, 'Abonnieren Sie unseren Newsletter (Abmeldung jederzeit möglich):', 'de_DE'),
 ('Mage_Customer::Sign Up for Newsletter', 0, 'In den Newsletter eintragen (Abmeldung jederzeit möglich)', 'de_DE'),
 ('Mage_Catalog::Availability: In stock.', 0, 'Verfügbarkeit: sofort lieferbar', 'de_DE'),
-('Mage_Sales::Tax', 0, 'Inkl. MwSt.', 'de_DE');
+('Mage_Sales::Tax', 0, 'Zzgl. MwSt.', 'de_DE'),
+('Mage_Sales::Subtotal', 0, 'Zwischensumme (Netto)', 'de_DE');
 EOF;
 $installer->run($query);
 
