344ab3639aa6ee02a3935e1d9250d5c74650873f
Eugen Gitin <devnull@localhost>
2009-06-02 23:54:57 +0200

    Dependency von den E-Mail Template Dateien eingetragen

--- info.py
+++ info.py
@@ -54,6 +54,41 @@ EXCLUDES = {
 }
 
 DEPENDS_ON_FILES = (
+    'app/locale/en_US/template/email/account_new.html',
+    'app/locale/en_US/template/email/account_new_confirmation.html',
+    'app/locale/en_US/template/email/account_new_confirmed.html',
+    'app/locale/en_US/template/email/admin_password_new.html',
+    'app/locale/en_US/template/email/amazonpayments_asp_notification_error.html',
+    'app/locale/en_US/template/email/contact_form.html',
+    'app/locale/en_US/template/email/currency_update_warning.html',
+    'app/locale/en_US/template/email/log_clean_warning.html',
+    'app/locale/en_US/template/email/newsletter_subscr_confirm.html',
+    'app/locale/en_US/template/email/newsletter_subscr_success.html',
+    'app/locale/en_US/template/email/newsletter_unsub_success.html',
+    'app/locale/en_US/template/email/password_new.html',
+    'app/locale/en_US/template/email/payment_failed.html',
+    'app/locale/en_US/template/email/product_alert_cron_error.html',
+    'app/locale/en_US/template/email/product_price_alert.html',
+    'app/locale/en_US/template/email/product_share.html',
+    'app/locale/en_US/template/email/product_stock_alert.html',
+    'app/locale/en_US/template/email/sitemap_generate_warning.html',
+    'app/locale/en_US/template/email/wishlist_share.html',
+    'app/locale/en_US/template/email/sales/creditmemo_new.html',
+    'app/locale/en_US/template/email/sales/creditmemo_new_guest.html',
+    'app/locale/en_US/template/email/sales/creditmemo_update.html',
+    'app/locale/en_US/template/email/sales/creditmemo_update_guest.html',
+    'app/locale/en_US/template/email/sales/invoice_new.html',
+    'app/locale/en_US/template/email/sales/invoice_new_guest.html',
+    'app/locale/en_US/template/email/sales/invoice_update.html',
+    'app/locale/en_US/template/email/sales/invoice_update_guest.html',
+    'app/locale/en_US/template/email/sales/order_new.html',
+    'app/locale/en_US/template/email/sales/order_new_guest.html',
+    'app/locale/en_US/template/email/sales/order_update.html',
+    'app/locale/en_US/template/email/sales/order_update_guest.html',
+    'app/locale/en_US/template/email/sales/shipment_new.html',
+    'app/locale/en_US/template/email/sales/shipment_new_guest.html',
+    'app/locale/en_US/template/email/sales/shipment_update.html',
+    'app/locale/en_US/template/email/sales/shipment_update_guest.html'
 )
 
 PEAR_KEY = ''
