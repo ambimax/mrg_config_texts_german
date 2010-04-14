cbd456042f35ed48a22192a2dc69dc2a76a86594
Eric Reiche <er@symmetrics.de>
2010-04-14 15:13:05 +0200

    ** Neue Migrationsskripte entfernt; 0.1.0 mit 0.3.0 überschrieben **

--- build/app/code/community/Symmetrics/ConfigGermanTexts/sql/config_german_texts_setup/mysql4-install-0.1.0.php
+++ build/app/code/community/Symmetrics/ConfigGermanTexts/sql/config_german_texts_setup/mysql4-install-0.1.0.php
@@ -39,7 +39,7 @@ foreach ($this->getConfigBlocks() as $name => $data) {
         if ($name == 'symmetrics_footerlinks') {
             $this->updateFooterLinksBlock($data);
         } else {
-            $this->createCmsBlock($data);
+            $this->createCmsBlock($data, true);
         }
     }
 }
@@ -51,48 +51,32 @@ foreach ($this->getConfigEmails() as $name => $data) {
     }
 }
 
-// set some translations
-$query = <<< EOF
-    INSERT INTO {$this->getTable('core_translate')} (`string`, `store_id`, `translate`, `locale`) VALUES
-    ('Mage_Sales::Logo for PDF print-outs (200x50)', 0, 'Logo for PDF print-outs', 'de_DE'),
-    ('Mage_Checkout::You will receive an order confirmation email with details of your order and a link to track its progress.', 0, 'Sie erhalten in kürze eine Bestellbestätigung per Email. Wenn Sie aktuell eingeloggt sind und einen Kunden Account in unserem Shop haben, dann klicken Sie unten auf den Link, um eine Kopie Ihrer Bestellbestätigung zu drucken.', 'de_DE'),
-    ('Mage_Checkout::Click <a href=""%s"" onclick=""this.target=''_blank''"">here to print</a> a copy of your order confirmation.', 0, '<a href=""%s"" onclick=""this.target=''_blank''"">Bestellbestätigung drucken</a>', 'de_DE'),
-    ('Mage_Checkout::Your order # is: <a href=""%s"">%s</a>', 0, 'Ihre Auftragsnummer lautet: <a href=""%s"">%s</a>', 'de_DE'),
-    ('Mage_Newsletter::Sign up for our newsletter:', 0, 'Abonnieren Sie unseren Newsletter (Abmeldung jederzeit möglich):', 'de_DE'),
-    ('Mage_Customer::Sign Up for Newsletter', 0, 'In den Newsletter eintragen (Abmeldung jederzeit möglich)', 'de_DE'),
-    ('Mage_Catalog::Availability: In stock.', 0, 'Verfügbarkeit: sofort lieferbar', 'de_DE'),
-    ('Mage_Sales::Tax', 0, 'Zzgl. MwSt.', 'de_DE'),
-    ('Mage_Checkout::Please agree to all Terms and Conditions before placing the order.', 0, 'Bitte bestätigen Sie die AGB und ggf. die Widerrufsbelehrung.', 'de_DE'),
-    ('Mage_Checkout::Please agree to all Terms and Conditions before placing the orders.', 0, 'Bitte bestätigen Sie die AGB und ggf. die Widerrufsbelehrung.', 'de_DE');
-EOF;
-
-$installer->run($query);
-
+$prefixNew = 'general/imprint/';
 // set imprint data
 $imprintFields = $this->getConfigImprint();
-$installer->setConfigData('general/impressum/company1', $imprintFields['company_name']);
-$installer->setConfigData('general/impressum/company2', $imprintFields['company_sub']);
-$installer->setConfigData('general/impressum/street', $imprintFields['street']);
-$installer->setConfigData('general/impressum/zip', $imprintFields['zip']);
-$installer->setConfigData('general/impressum/city', $imprintFields['city']);
-$installer->setConfigData('general/impressum/telephone', $imprintFields['phone']);
-$installer->setConfigData('general/impressum/email', $imprintFields['email']);
-$installer->setConfigData('general/impressum/fax', $imprintFields['fax']);
-$installer->setConfigData('general/impressum/web', $imprintFields['homepage']);
-$installer->setConfigData('general/impressum/taxnumber', $imprintFields['tax_number']);
-$installer->setConfigData('general/impressum/vatid', $imprintFields['sales_tax_id_number']);
-$installer->setConfigData('general/impressum/court', $imprintFields['commercial_register']);
-$installer->setConfigData('general/impressum/taxoffice', $imprintFields['tax_office']);
-$installer->setConfigData('general/impressum/ceo', $imprintFields['holder_names']);
-$installer->setConfigData('general/impressum/hrb', $imprintFields['hrb']);
-$installer->setConfigData('general/impressum/bankaccount', $imprintFields['bank_account']);
-$installer->setConfigData('general/impressum/bankcodenumber', $imprintFields['bank_id_code']);
-$installer->setConfigData('general/impressum/bankaccountowner', $imprintFields['bank_account_owner']);
-$installer->setConfigData('general/impressum/bankname', $imprintFields['bank_name']);
-$installer->setConfigData('general/impressum/swift', $imprintFields['bank_swift']);
-$installer->setConfigData('general/impressum/iban', $imprintFields['bank_iban']);
-$installer->setConfigData('general/impressum/shopname', $imprintFields['shop_name']);
-$installer->setConfigData('general/impressum/rechtlicheregelungen', $this->getTemplateContent($imprintFields['legal_info']));
+$installer->setConfigData($prefixNew . 'shop_name', $imprintFields['shop_name']);
+$installer->setConfigData($prefixNew . 'company_first', $imprintFields['company_name']);
+$installer->setConfigData($prefixNew . 'company_second', $imprintFields['company_sub']);
+$installer->setConfigData($prefixNew . 'street', $imprintFields['street']);
+$installer->setConfigData($prefixNew . 'zip', $imprintFields['zip']);
+$installer->setConfigData($prefixNew . 'city', $imprintFields['city']);
+$installer->setConfigData($prefixNew . 'telephone', $imprintFields['phone']);
+$installer->setConfigData($prefixNew . 'fax', $imprintFields['fax']);
+$installer->setConfigData($prefixNew . 'email', $imprintFields['email']);
+$installer->setConfigData($prefixNew . 'web', $imprintFields['homepage']);
+$installer->setConfigData($prefixNew . 'tax_number', $imprintFields['tax_number']);
+$installer->setConfigData($prefixNew . 'vat_id', $imprintFields['sales_tax_id_number']);
+$installer->setConfigData($prefixNew . 'court', $imprintFields['commercial_register']);
+$installer->setConfigData($prefixNew . 'financial_office', $imprintFields['tax_office']);
+$installer->setConfigData($prefixNew . 'ceo', $imprintFields['holder_names']);
+$installer->setConfigData($prefixNew . 'register_number', $imprintFields['hrb']);
+$installer->setConfigData($prefixNew . 'business_rules', $this->getTemplateContent($imprintFields['legal_info']));
+$installer->setConfigData($prefixNew . 'bank_account', $imprintFields['bank_account']);
+$installer->setConfigData($prefixNew . 'bank_code_number', $imprintFields['bank_id_code']);
+$installer->setConfigData($prefixNew . 'bank_account_owner', $imprintFields['bank_account_owner']);
+$installer->setConfigData($prefixNew . 'bank_name', $imprintFields['bank_name']);
+$installer->setConfigData($prefixNew . 'swift', $imprintFields['bank_swift']);
+$installer->setConfigData($prefixNew . 'iban', $imprintFields['bank_iban']);
 
 // set some misc data
 $installer->setConfigData('sales_pdf/invoice/put_order_id', '1');
