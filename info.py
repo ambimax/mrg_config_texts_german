67c82d9975d64b2ac6fc34f18e780ab23af515a6
Eric Reiche <er@symmetrics.de>
2010-05-04 15:54:00 +0200

    info.py korrigiert

--- info.py
+++ info.py
@@ -20,18 +20,17 @@ INSTALL_PATH = ''
 # responsibilities
 # =============================================================================
 TEAM_LEADER = {
-    'Sergej Braznikov': 'sb@symmetrics.de',
-    'Torsten Walluhn': 'tw@symmetrics.de'
+    'Torsten Walluhn': 'tw@symmetrics.de',
 }
 
 MAINTAINER = {
     'Eugen Gitin': 'eg@symmetrics.de',
-    'Eric Reiche': 'er@symmetrics.de'
+    'Eric Reiche': 'er@symmetrics.de',
 }
 
 AUTHORS = {
     'Eugen Gitin': 'eg@symmetrics.de',
-    'Eric Reiche': 'er@symmetrics.de'
+    'Eric Reiche': 'er@symmetrics.de',
 }
 
 # =============================================================================
@@ -52,52 +51,54 @@ NOTES = '''
 REQUIRES = [
     {'magento': '*', 'magento_enterprise': '*'}, 
     {'mc_module_locale_mage_community_de_de': '*'}, 
-    {'symmetrics_config_german': '*'}
+    {'symmetrics_config_german': '*'},
 ]
 
 EXCLUDES = {}
 
+email_template = 'app/locale/en_US/template/email/'
+
 DEPENDS_ON_FILES = (
-    'app/locale/en_US/template/email/account_new.html',
-    'app/locale/en_US/template/email/account_new_confirmation.html',
-    'app/locale/en_US/template/email/account_new_confirmed.html',
-    'app/locale/en_US/template/email/admin_password_new.html',
-    'app/locale/en_US/template/email/amazonpayments_asp_notification_error.html',
-    'app/locale/en_US/template/email/contact_form.html',
-    'app/locale/en_US/template/email/currency_update_warning.html',
-    'app/locale/en_US/template/email/log_clean_warning.html',
-    'app/locale/en_US/template/email/newsletter_subscr_confirm.html',
-    'app/locale/en_US/template/email/newsletter_subscr_success.html',
-    'app/locale/en_US/template/email/newsletter_unsub_success.html',
-    'app/locale/en_US/template/email/password_new.html',
-    'app/locale/en_US/template/email/payment_failed.html',
-    'app/locale/en_US/template/email/product_alert_cron_error.html',
-    'app/locale/en_US/template/email/product_price_alert.html',
-    'app/locale/en_US/template/email/product_share.html',
-    'app/locale/en_US/template/email/product_stock_alert.html',
-    'app/locale/en_US/template/email/sitemap_generate_warning.html',
-    'app/locale/en_US/template/email/wishlist_share.html',
-    'app/locale/en_US/template/email/sales/creditmemo_new.html',
-    'app/locale/en_US/template/email/sales/creditmemo_new_guest.html',
-    'app/locale/en_US/template/email/sales/creditmemo_update.html',
-    'app/locale/en_US/template/email/sales/creditmemo_update_guest.html',
-    'app/locale/en_US/template/email/sales/invoice_new.html',
-    'app/locale/en_US/template/email/sales/invoice_new_guest.html',
-    'app/locale/en_US/template/email/sales/invoice_update.html',
-    'app/locale/en_US/template/email/sales/invoice_update_guest.html',
-    'app/locale/en_US/template/email/sales/order_new.html',
-    'app/locale/en_US/template/email/sales/order_new_guest.html',
-    'app/locale/en_US/template/email/sales/order_update.html',
-    'app/locale/en_US/template/email/sales/order_update_guest.html',
-    'app/locale/en_US/template/email/sales/shipment_new.html',
-    'app/locale/en_US/template/email/sales/shipment_new_guest.html',
-    'app/locale/en_US/template/email/sales/shipment_update.html',
-    'app/locale/en_US/template/email/sales/shipment_update_guest.html'
+    email_template + 'account_new.html',
+    email_template + 'account_new_confirmation.html',
+    email_template + 'account_new_confirmed.html',
+    email_template + 'admin_password_new.html',
+    email_template + 'amazonpayments_asp_notification_error.html',
+    email_template + 'contact_form.html',
+    email_template + 'currency_update_warning.html',
+    email_template + 'log_clean_warning.html',
+    email_template + 'newsletter_subscr_confirm.html',
+    email_template + 'newsletter_subscr_success.html',
+    email_template + 'newsletter_unsub_success.html',
+    email_template + 'password_new.html',
+    email_template + 'payment_failed.html',
+    email_template + 'product_alert_cron_error.html',
+    email_template + 'product_price_alert.html',
+    email_template + 'product_share.html',
+    email_template + 'product_stock_alert.html',
+    email_template + 'sitemap_generate_warning.html',
+    email_template + 'wishlist_share.html',
+    email_template + 'sales/creditmemo_new.html',
+    email_template + 'sales/creditmemo_new_guest.html',
+    email_template + 'sales/creditmemo_update.html',
+    email_template + 'sales/creditmemo_update_guest.html',
+    email_template + 'sales/invoice_new.html',
+    email_template + 'sales/invoice_new_guest.html',
+    email_template + 'sales/invoice_update.html',
+    email_template + 'sales/invoice_update_guest.html',
+    email_template + 'sales/order_new.html',
+    email_template + 'sales/order_new_guest.html',
+    email_template + 'sales/order_update.html',
+    email_template + 'sales/order_update_guest.html',
+    email_template + 'sales/shipment_new.html',
+    email_template + 'sales/shipment_new_guest.html',
+    email_template + 'sales/shipment_update.html',
+    email_template + 'sales/shipment_update_guest.html',
 )
 
 PEAR_KEY = ''
 
 COMPATIBLE_WITH = {
     'magento': ['1.4.0.0'],
-	'magento_enterprise': ['1.7.0.0']
+    'magento_enterprise': ['1.7.0.0'],
 }
